52e2a0fbe22c3244da0b804e345997fc6a045433
[your <[Your>
2022-12-16 22:20:39 -0300

    criando componentes

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import LoginPage from "./pages/LoginPage/LoginPage"
+import CreateAccountPage from "./pages/CreateAccountPage/CreateAccountPage"
+import HomePage from "./pages/HomePage/HomePage"
+import GoalDayPage from "./pages/GoalDayPage/GoalDayPage"
+import HistoricGoalsPage from "./pages/HistoricGoalsPage/HistoricGoalsPage"
+
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function App() {
+
+  return (   
+    <BrowserRouter>
+
+    <Routes>
+
+      <Route path="/" element={<LoginPage/>}/>
+      <Route path="/cadastro" element={<CreateAccountPage/>}/>
+      <Route path="/habitos" element={<HomePage/>}/>
+      <Route path="/hoje" element={<GoalDayPage/>}/>
+      <Route path="/historico" element={<HistoricGoalsPage/>}/>
+      
+      </Routes>
+    
+    </BrowserRouter>
+
+    )
+    }
